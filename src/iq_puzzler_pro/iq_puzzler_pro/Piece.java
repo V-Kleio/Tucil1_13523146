42bc77de4878c43ce552af6ec2d0775f91d00129
Vylon-Kleio <rafaelmarchel.dw@gmail.com>
2025-02-22 02:51:22 +0700

    feat: Piece can be rotated and mirrored

--- src/iq_puzzler_pro/iq_puzzler_pro/Piece.java
+++ src/iq_puzzler_pro/iq_puzzler_pro/Piece.java
@@ -2,20 +2,48 @@ package iq_puzzler_pro;
 
 public class Piece {
     private final char[][] shape;
+    private final int row;
+    private final int col;
 
     public Piece(char[][] shape) {
         this.shape = shape;
+        this.row = shape.length;
+        this.col = shape[0].length;
     }
 
     public char[][] getShape() {
         return shape;
     }
 
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
     public Piece rotate() {
-        return this;
+        // * This will rotate the piece ccw
+
+        char[][] rotatedPiece = new char[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                rotatedPiece[col - 1 - j][i] = shape[i][j];
+            }
+        }
+
+        return new Piece(rotatedPiece);
     }
 
     public Piece mirror() {
-        return this;
+        char[][] mirroredPiece = new char[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                mirroredPiece[i][col - 1 - j] = shape[i][j];
+            }
+        }
+
+        return new Piece(mirroredPiece);
     }
 }
